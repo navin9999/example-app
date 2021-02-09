31101610143a0a764aab7aff6f175604c0b664bf
unknown <Administrator@pc23.RV.COM>
2021-02-09 19:14:53 +0530

    'lastchanges'

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -66,6 +66,16 @@
 											<li class="menu-item" >
 												<a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
 											</li>
+											<li class="menu-item" >
+												<a title="Logout" href="{{ route('logout') }}"
+												 onclick="event.preventDefault();  document.getElementById('logout-form').submit();" >Logout</a>
+											</li>
+											<form  id="logout-form" method="POST" action="{{ route('logout') }}">
+												@csrf
+											
+
+											</form>
+
 										</ul>
 									</li>
 
@@ -78,6 +88,15 @@
 											<li class="menu-item" >
 											<a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a>
 											</li>
+											<li class="menu-item" >
+												<a title="Logout" href="{{ route('logout') }}"
+												 onclick="event.preventDefault(); document.getElementById('logout-form').submit();" >Logout</a>
+											</li>
+											<form  id="logout-form" method="POST" action="{{ route('logout') }}">
+												@csrf
+											
+
+											</form>
 										</ul>
 									</li>
 
